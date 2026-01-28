395ce2cc851f25dc05c9414cc102e70484fa21bd
Finlay Williams <finlayw@palantir.com>
2026-01-28 15:07:21 +0000

    tidy

--- gradle-plugin-testing-junit/src/main/java/com/palantir/gradle/testing/junit/GradleVersions.java
+++ gradle-plugin-testing-junit/src/main/java/com/palantir/gradle/testing/junit/GradleVersions.java
@@ -25,7 +25,6 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.platform.commons.support.AnnotationSupport;
 
@@ -89,8 +88,7 @@ final class GradleVersions {
     }
 
     private static boolean isTestMethod(Method method) {
-        return AnnotationSupport.isAnnotated(method, Test.class)
-                || AnnotationSupport.isAnnotated(method, TestTemplate.class);
+        return AnnotationSupport.isAnnotated(method, Test.class);
     }
 
     private GradleVersions() {}
