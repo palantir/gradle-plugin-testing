5b10e5ac40a91c142f500f3d07a137ca401de3bd
FinlayRJW <88376465+FinlayRJW@users.noreply.github.com>
2026-03-11 13:53:52 +0000

    Update gradle-plugin-testing-junit/src/main/java/com/palantir/gradle/testing/project/GradleProject.java
    Co-authored-by: Claudia Rogoz <claudia.rogoz93@gmail.com>

--- gradle-plugin-testing-junit/src/main/java/com/palantir/gradle/testing/project/GradleProject.java
+++ gradle-plugin-testing-junit/src/main/java/com/palantir/gradle/testing/project/GradleProject.java
@@ -56,8 +56,8 @@ public interface GradleProject extends Directory {
     }
 
     private static Path createValidatedDirectory(Path parent, String name, String type) {
-        Preconditions.checkArgument(!name.contains(":"), "%s names must not %s contain colons", type, name);
-        Preconditions.checkArgument(!name.contains("/"), "%s names must not %s contain slashes", type, name);
+        Preconditions.checkArgument(!name.contains(":"), "%s names must not contain colons (project name was %s)", type, name);
+        Preconditions.checkArgument(!name.contains("/"), "%s names must not contain slashes (project name was %s)", type, name);
 
         Path dir = parent.resolve(name);
 
